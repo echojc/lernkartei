cd3864adb910642a764c2272b0f2620fe144c05f
Jonathan Chow <me@echo.sh>
2017-07-26 22:23:35 +1200

    Output result as json

--- server.go
+++ server.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
+	"github.com/echojc/lernkartei/dict"
 	"github.com/echojc/lernkartei/dict/glosbe"
 )
 
 func main() {
+	var out []dict.Word
 	for _, arg := range os.Args[1:] {
 		words, _ := glosbe.NewWord(arg)
-		for _, word := range words {
-			fmt.Printf("%+v\n", word)
-		}
+		out = append(out, words...)
 	}
+
+	bs, _ := json.Marshal(out)
+	fmt.Println(string(bs))
 }
