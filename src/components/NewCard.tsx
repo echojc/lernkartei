b60dba639f8ef7ad1c2c8bee74fee25c43317f92
Jonathan Chow <me@echo.sh>
2017-07-29 17:54:32 +1200

    Render search results

--- src/components/NewCard.tsx
+++ src/components/NewCard.tsx
@@ -4,12 +4,85 @@ import { Card } from './Card';
 
 import * as styles from './newCard.less';
 
+interface Result {
+  Base: string;
+  Definitions: string[];
+  Forms: string[];
+}
+
+const results = [
+  {
+    Base: 'lesen',
+    Definitions: [
+      'read',
+      'pick',
+      'lecture',
+      'get',
+    ],
+    Forms: [
+      'liest',
+      'las',
+      'gelesen haben',
+    ],
+  },
+  {
+    Base: 'Hund',
+    Definitions: [
+      'dog',
+      'hound',
+      'canid',
+      'scoundrel',
+    ],
+    Forms: [
+      'der Hund',
+      'die Hunde',
+    ],
+  },
+  {
+    Base: 'gut',
+    Definitions: [
+      'good',
+      'well',
+      'all right',
+      'fine',
+    ],
+    Forms: [
+      'besser',
+      'am besten',
+    ],
+  },
+];
+
+function renderResult(r: Result): React.ReactElement<{}> {
+  const forms = r.Forms
+    ? <span className={styles.extended}>({r.Forms.join(', ')})</span>
+    : null;
+
+  const defs = r.Definitions
+    ? r.Definitions.join(', ')
+    : null;
+
+  return (
+    <section className={styles.result} key={r.Base}>
+      <dl>
+        <dt>{r.Base} {forms}</dt>
+        <dd>{defs}</dd>
+      </dl>
+    </section>
+  );
+}
+
 export class NewCard extends React.Component<{}, {}> {
   render() {
     return (
       <Card
         front={
-          <input className={styles.input} />
+          <div className={styles.search}>
+            <input className={styles.input} placeholder={'Suchen...'} />
+            <div className={styles.results}>
+              {results.map(renderResult)}
+            </div>
+          </div>
         }
         disableFlip={true}
       />
